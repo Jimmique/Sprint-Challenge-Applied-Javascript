4bcffd547d22dc8a84aae77e86e07cc4d565aed8
Jimmique <jimmique88@gmail.com>
2019-07-19 19:39:14 -0700

    Completed Tab component/section

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,7 +8,15 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+const topicsContainer = document.querySelector('.topics');
 
+ function Tab(topic) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.innerText = topic;
+
+   return tab;
+}
 
 const url = 'https://lambda-times-backend.herokuapp.com/topics'
 axios.get(url)
